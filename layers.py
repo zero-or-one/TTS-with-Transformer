bfbaf264a83463dd834781f207c19bc4582b865d
zero-or-one <48153370+zero-or-one@users.noreply.github.com>
2022-11-13 23:29:55 +0900

    begin

---
+++ layers.py
@@ -0,0 +1,46 @@
+import torch
+
+
+class LinearNorm(torch.nn.Module):
+    def __init__(self, in_dim, out_dim, bias=True, w_init='lienar'):
+        '''
+        Linear layer with normal initialization
+        to avoid large or small weights
+        -> in_dim: dimension of input
+        -> out_dim: dimension of output
+        -> bias: boolean wheither to add bias term to weights
+        -> w_init: xavier initialization weight gain
+        '''
+        super(LinearNorm, self).__init__()
+        self.linear = torch.nn.Linear(in_dim, out_dim, bias=bias)
+        torch.nn.init.xavier_uniform_(
+            self.liner.weight,
+            gain=torch.nn.init.calculate_gain(w_init))
+    
+    def forward(self, x):
+        return self.linear(x)
+
+
+class ConvNorm(torch.nn.Module):
+    def __init__(self, in_ch, out_ch, kernel_size=1, stride=1,
+        padding=0, dilation=1, bias=True, w_init='lienar'):
+        '''
+        1D Convolution layer with normal initialization
+        to avoid large or small weights
+        -> in_ch: channels of input
+        -> in_dim: channels of output
+        -> kernel_size: convolution window size
+        -> stride: step size
+        -> padding: size of the padding for input
+        -> dilation: dilation rate
+        -> bias: boolean wheither to add bias term to weights
+        -> w_init: xavier initialization weight gain
+        ''' 
+        self.conv = torch.nn.Conv1d(in_ch, out_ch, kernel_size,
+            stride, padding, dilation, bias=bias)
+        torch.nn.init.xavier_uniform_(
+            self.conv.weight,
+            gain=torch.nn.init.calculate_gain(w_init))    
+
+    def forward(self, x):
+        return self.conv(x)    
